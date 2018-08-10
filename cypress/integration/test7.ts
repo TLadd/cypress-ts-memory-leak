e1ec836afe7d6b3964c0fa56dd1fe897e61b6a29
Thomas Ladd <thomas.ladd@stackpath.com>
2018-08-10 01:11:31 -0500

    Test adding 50 tests per test file. OOM occurs on 31st test file instead of 37th

--- cypress/integration/test7.ts
+++ cypress/integration/test7.ts
@@ -1,3 +1,150 @@
 it("is a simple test", () => {
   expect(1).to.eq(1);
 });
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
+it("is a simple test", () => {
+  expect(1).to.eq(1);
+});
